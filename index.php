df4ec608e4665a2c27858434f2b63eec8d397c76
Ayman <a.m_ayman9@outlook.com>
2023-09-17 17:52:48 +0300

    PHP: Define Variables

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+<!--  Define Variables  -->
+
+    <h1>
+        <?php
+            $greeting = "Hello";
+//            echo $greeting . " " . "Everybody!";
+            echo "$greeting Everybody!";
+        ?>
+    </h1>
+
+
+
+
+
+</body>
+</html>
