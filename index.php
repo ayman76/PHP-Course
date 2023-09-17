083781eda659b6341f91ab497350cddf3221b82b
Ayman <a.m_ayman9@outlook.com>
2023-09-17 18:26:31 +0300

    PHP: Arrays and Loops

--- index.php
+++ index.php
@@ -1,36 +1,27 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
+	<meta charset="UTF-8">
+	<title>Demo</title>
 
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        $name = "Dark Matter";
-        $read = true;
 
-        if ($read) {
-            $message = "You have read $name";
-        } else {
-            $message = "You have NOT read $name";
-        }
+	<h1>Recommended Books</h1>
+
+    <?php
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
 
-    <h1>
-        <!--        --><?php //echo $message;?>
-        <?= $message ?>
-    </h1>
-
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 
 </body>
 </html>
