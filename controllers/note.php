d4861f4e3cd040cca59971219cbc61a7f637e228
Ayman <a.m_ayman9@outlook.com>
2023-09-19 18:01:02 +0300

    PHP: Introduction to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -2,9 +2,17 @@
 
 $config = require 'config.php';
 $db = new Database($config['database']);
+$currentUserId = 1;
 
 $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();
 
+if(!$note){
+    abort();
+}
+
+if ($note['user_id'] !== $currentUserId){
+    abort(Response::FORBIDDEN);
+}
 
 $heading = "Note";
 
--- index.php
+++ index.php
@@ -2,26 +2,5 @@
 
 require 'functions.php';
 require 'Database.php';
+require 'Response.php';
 require 'router.php';
-
-//Connect to our MySQL database
-
-
-
-$id = $_GET['id'];
-
-//Don't inline user parameter in sql query this will help user to inject sql queries in your query like drop tables
-//$query = "select * from posts where  id = {$id}";
-
-//We fix it by adding ? or :nameOfCol that we need to get its value from user and pass it as parameter in execute method in Database class
-
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
-
-//foreach ($posts as $post){
-//    echo "<li>" . $post['title'] . "</li>";
-//}
-
