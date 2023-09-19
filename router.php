d65ea66713a6e3aa051634b256c1947d04af1d92
Ayman <a.m_ayman9@outlook.com>
2023-09-19 19:03:26 +0300

    PHP: Extract routes in file and make create note page containing form

--- router.php
+++ router.php
@@ -1,16 +1,4 @@
 <?php
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-
 function routeToController($uri, $routes){
     if(array_key_exists($uri, $routes)){
         require $routes[$uri];
@@ -27,5 +15,6 @@ function abort($code = 404){
     die();
 }
 
-
+$routes = require 'routes.php';
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 routeToController($uri, $routes);
