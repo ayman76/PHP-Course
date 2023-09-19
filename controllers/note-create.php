e509e3589ddef41a9fd9746372356c9cc3bd5895
Ayman <a.m_ayman9@outlook.com>
2023-09-20 00:32:44 +0300

    PHP: Extract Validator Class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'Validator.php';
 
 $heading = "New Note";
 
@@ -9,14 +9,10 @@ $db = new Database($config['database']);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen(trim($_POST['body'])) === 0) {
-        $errors['body'] = 'A body is required';
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
 
-    if(strlen($_POST['body'])> 1000){
-        $errors['body'] = "The body field can't be more than 1,000 characters.";
-    }
-    
     if (empty($errors)) {
         $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
             'body' => $_POST['body'],
