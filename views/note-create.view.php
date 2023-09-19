f52e6abfd2b287512a6d52be100d4961c363011d
Ayman <a.m_ayman9@outlook.com>
2023-09-20 00:12:56 +0300

    PHP: Adding Basic Form validation

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -13,9 +13,18 @@
                     <div class="col-span-full">
                         <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
                         <div class="mt-2">
-                                <textarea id="body" name="body" rows="3" placeholder="Here's an idea for a note..."
-                                          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                <textarea
+                                        id="body"
+                                        name="body"
+                                        rows="3"
+                                        placeholder="Here's an idea for a note..."
+                                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                ><?= $_POST['body'] ?? '' ?></textarea>
                         </div>
+
+                        <?php if (isset($errors['body'])): ?>
+                            <p class="text-red-500 text-xs mt-3"><?= $errors['body'] ?></p>
+                        <?php endif ?>
                     </div>
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
