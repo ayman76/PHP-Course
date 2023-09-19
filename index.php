697bcfedf51ae942206f752311bb76ae9adcc530
Ayman <a.m_ayman9@outlook.com>
2023-09-19 13:24:20 +0300

    PHP: Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
 
 require 'functions.php';
-
+require 'Database.php';
 //require 'router.php';
 
 //Connect to our MySQL database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
+$db = new Database();
 
-$statement = $pdo->prepare("select * from posts");
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
 
 //dd($posts);
 
